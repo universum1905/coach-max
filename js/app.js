05831cea4f3a729cb99f1b377087538a154f51a2
universum1905 <evren-oezerkan@web.de>
2025-07-04 18:34:37 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1128,8 +1128,8 @@ box.appendChild(btn);
   const img = btn.querySelector("img");
   img.style.boxShadow = "0 0 12px 4px #ffd54f";
   img.style.transform = "scale(1.1)";
-  img.style.marginBottom = "8px";
-img.style.marginTop = "20px";
+  img.style.marginTop = "0";
+img.style.marginBottom = "-10px";
   img.style.transition = "all 0.4s ease";
 
   // ✅ 3. Kleines „Yay!“ darunter (kindlich, zentriert)
