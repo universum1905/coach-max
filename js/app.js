d558772fa34aeb02193f880da392ba53c1d58cb5
universum1905 <evren-oezerkan@web.de>
2025-07-07 20:18:48 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -728,10 +728,13 @@ document.body.appendChild(btn);
     videoElement.poster = "images/video-placeholder.png";
     videoElement.className = "session-video";
 
-    videoBox = document.createElement("div");
+    videoBox = document.createElement('div');
     videoBox.className = "floating-video";
-    videoBox.style.zIndex = "1000"; // Höchster Wert, bleibt IMMER oben
-    videoBox.appendChild(video);
+    videoBox.style.position = "fixed";
+    videoBox.style.right = "22px";
+    videoBox.style.bottom = "45px";
+    videoBox.style.zIndex = "1000";
+    videoBox.appendChild(videoElement);
     document.body.appendChild(videoBox);
 
     // --- Play-Button Overlay ---
