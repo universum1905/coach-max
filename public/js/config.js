ec1d71fee908235f0c9e89e1f61d4fc7855c315e
universum1905 <evren-oezerkan@web.de>
2025-07-27 15:02:53 +0200

    1

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,4 @@
-// === Globaler Schalter ===
-export const DEV_MODE = true;       // true = Testmodus, false = Live
-export const DEV_START_SESSION = 2; // Index der Session im DEV-Modus
-export const DEV_DAY = 1;           // Welcher Tag im DEV-Modus geladen wird
+// /js/config.js
+export const DEV_MODE = true;            // Umschalten: true = Testmodus, false = Live
+export const DEV_START_SESSION = 2;      // Index der Session (0 = Intro)
+export const DEV_DAY = 1;                // Tag im Testmodus
